3458a9f12505cb9d675948b5fbae98b235325436
Harrison <harrisonpinchieri@hotmail.com>
2018-03-02 14:01:37 -0300

    Initial commit

---
+++ app/src/main/java/projetoactivity/cursoandroid/com/projetoactivity/SegundoActivity.java
@@ -0,0 +1,13 @@
+package projetoactivity.cursoandroid.com.projetoactivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SegundoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segundo);
+    }
+}
